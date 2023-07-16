bfa547506160130e012052f9ff788cfde769a241
Raúl Díaz <127735127+RaulDdrt@users.noreply.github.com>
2023-07-16 14:20:04 +0100

    reto 3 hecho

--- Dia3/indexMarks.js
+++ Dia3/indexMarks.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 const newMark = require("./marksModel")
+const newTeacher = require("./teachersModel")
+const teachersModel = require("./teachersModel")
 
 
 mongoose.connect("mongodb+srv://rauldiazdrt:mymongo123@rauld.nhvo4lb.mongodb.net/" ,
@@ -7,22 +9,102 @@ mongoose.connect("mongodb+srv://rauldiazdrt:mymongo123@rauld.nhvo4lb.mongodb.net
 })
 
 
-let markDocument = new newMark({
-    id: 10,
-    date: 1993,
-    mark: 9,
-    student_first_name: "raul",
-    student_last_name: "diaz",
-    group_name: "tarde",
-    subject_name: "SQL",
-    teachers: ["erika", "juan"]
+let teacherSchema = new mongoose.Schema({
+    teacher_first_name: String,
+    teacher_last_name: String,
 })
 
-markDocument.save()
-    .then(() => {
-        console.log("Documento guardado");
-        mongoose.disconnect();
+let markSchema = new mongoose.Schema({
+    id: Number,
+    date: Number,
+    mark: Number,
+    student_first_name: String,
+    student_last_name: String,
+    group_name: String,
+    subject_name: String,
+    teachers: [teacherSchema]
+})
+
+
+
+// newMark.aggregate([{$project: {"Date": "$date",
+//                             student_first_name: "pedro",
+//                         }}])
+// .then((result)=>{
+//     console.log(result)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+// newMark.aggregate([{$match:{subject_name: "MongoDB",}},{
+//                     $group: {_id: null, notaMedia: {$avg: "$mark"}},
+// }])
+// .then((result) =>{
+//     console.log(result)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+// newMark.countDocuments({})
+// .then((alumnos)=>{
+//     console.log("Hay " + alumnos + " alumnos en el bootcamp")
+// })
+// .catch((error) =>{
+//     console.log(error)
+// })
+
+// newMark.find({}, "student_first_name , student_last_name")
+// .then((alumnos)=>{
+//     alumnos.forEach(alumno=>{
+//         console.log(alumno.student_first_name, alumno.student_last_name)
+//     })
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+// newMark.aggregate([{$group: {
+//                             _id: "$group_name",
+//                             alumnos: { $sum: 1 }
+// }},
+// {
+//                             $sort: {
+//                             _id: -1
+// }}])
+// .then((result)=>{
+//     result.forEach((grupo)=>{
+//         console.log(grupo._id, grupo.alumnos)
+//     })
+// })
+// .catch((error) =>{
+//     console.log(error)
+// })
+
+newMark.aggregate([{$group: {
+                    _id: "$subject_name",
+                    media: { $avg: "$mark" }
+}
+},
+                {
+                $match: {
+                media: { $gt: 5 }
+}
+},
+                {
+                $sort: {
+                media: -1
+}
+},
+                {
+                $limit: 5
+}])
+.then((result)=>{
+    result.forEach(subject =>{
+        console.log(subject._id)
     })
-    .catch((err) => {
-        console.log("Error: " + err)
-});
+})
+.catch((error) =>{
+    console.log(error)
+})
--- Dia3/indexTeachers.js
+++ Dia3/indexTeachers.js
@@ -3,20 +3,43 @@ const newTeacher = require("./teachersModel")
 
 
 mongoose.connect("mongodb+srv://rauldiazdrt:mymongo123@rauld.nhvo4lb.mongodb.net/" ,
-                {useNewUrlParser: false, useUnifiedTopology: false
+                {useNewUrlParser: true, useUnifiedTopology: true
 })
 
-
-let teacherDocument = new newTeacher({
-    teacher_first_name: "juan",
-    teacher_last_name: "gomez"
+let teacherSchema = new mongoose.Schema({
+    teacher_first_name: String,
+    teacher_last_name: String,
 })
 
-teacherDocument.save()
-    .then(() => {
-        console.log("Documento guardado");
-        mongoose.disconnect();
-    })
-    .catch((err) => {
-        console.log("Error: " + err)
-});
+// let teacherDocument = new newTeacher({
+//     teacher_first_name: "juan",
+//     teacher_last_name: "gomez"
+// })
+
+// teacherDocument.save()
+//     .then(() => {
+//         console.log("Documento guardado");
+//         mongoose.disconnect();
+//     })
+//     .catch((err) => {
+//         console.log("Error: " + err)
+// });
+
+// newTeacher.find({}, "teacher_first_name , teacher_last_name")
+// .then((profes)=>{
+//     profes.forEach(profe=>{
+//         console.log(profe.teacher_first_name, profe.teacher_last_name)
+//     })
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+newTeacher.countDocuments({})
+.then((teachers)=>{
+    console.log("Hay " + teachers + " profesores en el bootcamp")
+})
+.catch((error) =>{
+    console.log(error)
+})
+
